7de3f092067b055006ec4c7de7cc20c31d86eed0
cstegall07 <charmainestegall@Charmaines-MacBook-Air.local>
2022-03-29 20:16:05 -0400

    complete

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,5 +1,18 @@
+import  BlogArticle  from './BlogArticle';
+
 const Blogs = () => {
-    return <h1>Blog Articles</h1>;
+
+    const blogArticles = [
+        {id: 1, title: "A title 1", author: "An author 1", article: "An article 1"},
+        {id: 2, title: "A title 2", author: "An author 2", article: "An article 2"}
+    ];
+
+    return (
+    <>
+        <h1>Blog Articles</h1>
+        {blogArticles.map((blogPost) => <BlogArticle key={blogPost.id} post = {blogPost}/> )}
+    </>
+    )
   };
   
   export default Blogs;
